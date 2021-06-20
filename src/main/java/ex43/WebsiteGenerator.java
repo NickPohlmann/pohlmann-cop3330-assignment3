867c0b8825420ad6f9b0b6e9514a7415d2c5f6b8
nicholaspohlmann <pohlmann.nick@gmail.com>
2021-06-20 17:41:24 -0400

    finished ex 43

--- src/main/java/ex43/WebsiteGenerator.java
+++ src/main/java/ex43/WebsiteGenerator.java
@@ -5,6 +5,9 @@
 
 package ex43;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.io.File;
 import java.lang.Object;
@@ -12,23 +15,44 @@ import java.lang.Object;
 public class WebsiteGenerator {
     static Scanner in = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         WebsiteGenerator websiteGenerator = new WebsiteGenerator();
 
         String websiteName = websiteGenerator.getInputNames("Site name: ");
         String authorName = websiteGenerator.getInputNames("Author name: ");
         boolean javaScriptFolder = websiteGenerator.getInputFolder("Do you want a folder for JavaScript (y/n)? ");
         boolean cssFolder = websiteGenerator.getInputFolder("Do you want a folder for CSS (y/n)? ");
-
-        File file = new File(String.format("%s",websiteName));
-        boolean bool = file.mkdir();
-        String currentDirectory = System.getProperty("user.dir");
-
-        File js = new File(String.format("%s/%s/js", currentDirectory,websiteName));
-        js.mkdir();
-        System.out.print(bool);
+        websiteGenerator.createWebsite(websiteName, authorName, javaScriptFolder, cssFolder);
     }
 
+    private void createWebsite(String websiteName, String authorName, boolean javaScriptFolder, boolean cssFolder) throws Exception {
+        File file = new File(String.format("%s",websiteName));
+        String currentDirectory = System.getProperty("user.dir");
+        file.mkdir();
+
+        createHTMLFile(websiteName, authorName);
+        System.out.print(javaScriptFolder);
+        System.out.print(cssFolder);
+        if (javaScriptFolder){
+            createFolder(currentDirectory, websiteName, "js");
+        }
+        if (cssFolder) {
+            createFolder(currentDirectory, websiteName, "css");
+        }
+    }
+
+    private void createFolder(String currentDirectory, String websiteName, String folderName) {
+        File js = new File(String.format("%s/%s/%s", currentDirectory,websiteName, folderName));
+        js.mkdir();
+    }
+
+    private void createHTMLFile(String websiteName, String authorName) throws IOException {
+        File file = new File(String.format("%s/index.html",websiteName));
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fileWriter);
+        bw.write(String.format("<html><title><h1> %s </h1><meta name=%s content=%s></html>", websiteName, "author", authorName));
+        bw.close();
+    }
 
     private boolean getInputFolder(String str) {
         System.out.print(str);
